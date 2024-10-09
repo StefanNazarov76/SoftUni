9b140caba7be9bc4733ee9fa24dbb98fbf8b785f
StefanNazarov76 <stefannazarov76@gmail.com>
2024-10-09 20:20:20 +0300

    more tasks

---
+++ python_advanced/06_multidimensional_lists_exercise/06_matrix_shuffling.py
@@ -0,0 +1,27 @@
+def check_coordinates(indices):
+    return {indices[0], indices[2]}.issubset(valid_rows) and {indices[1], indices[3]}.issubset(valid_cols)
+
+
+def swap_elements(command, indices):
+    if len(indices) == 4 and check_coordinates(indices) and command == 'swap':
+        row1, col1, row2, col2 = indices
+        matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+
+        [print(*row) for row in matrix]
+    else:
+        print('Invalid input!')
+
+
+rows, cols = [int(el) for el in input().split()]
+
+matrix = [[int(el) for el in input().split()] for _ in range(rows)]
+valid_rows = range(rows)
+valid_cols = range(cols)
+
+while True:
+    command_type, *coordinates = [int(el) if el.isdigit() else el for el in input().split()]
+
+    if command_type == 'END':
+        break
+
+    swap_elements(command_type, coordinates)
